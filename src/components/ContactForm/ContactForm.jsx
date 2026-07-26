b3151bc481b08885d64da032f0e9bbb4fb314c4b
Jack Pachon <jack.kilian3592@gmail.com>
2026-07-26 15:40:03 -0500

    cambio en la importacion de Contactform.css

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import "./ContactForm.css";
+import "./Contactform.css";
 
 export default function ContactForm() {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
     message: "",
-    
+
   });
   const [submitted, setSubmitted] = useState(false);
 
